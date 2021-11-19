72800e80257f35cc6158acdf9509f77bc33f119a
jasonjosephtjiadi@gmail.com <jasonjosephtjiadi@gmail.com>
2021-11-19 19:12:58 +0700

    First upload

---
+++ foo.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 19:01:31 2021
+
+@author: jason
+"""
+
+def is_divisible_by_k(x,k):
+    #Checks whether x is divisible by k    
+    assert x%k == 0
+    
+# Store all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding doubles)
+
+x = ()
+for i in range(1000):
+    if (is_divisible_by_k(x,2) & is_divisible_by_k(x, 3) | is_divisible_by_k(x,7):
+    x.append(i)
+    
+#Sum all integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding doubles)
+
+sum(x)
